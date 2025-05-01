8c141021a74424f68cf4a5d3f16ae62caaf2e368
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-01 10:09:25 -0400

    Update piglatin.exs

---
+++ piglatin.exs
@@ -0,0 +1,22 @@
+
+defmodule Pig_latin do
+  def is_vowel(c) do
+    case Enum.member?([~c"a", ~c"A", ~c"e", ~c"i", ~c"o", ~c"u"], c) do 
+      :ok -> true
+      :error -> false
+      _ -> false
+    end
+  end
+  def pigify(w) do
+    case is_vowel(String.first(w)) do 
+      :ok -> w <> "yay"
+      :error -> "Error"
+      _ -> String.slice(w, 1..-1//1) <> String.first(w) <> "ay"
+    end
+  end
+end
+
+result1 = Pig_latin.pigify("Jeremy")
+result2 = Pig_latin.pigify("Aaron")
+IO.puts(result1)
+IO.puts(result2)

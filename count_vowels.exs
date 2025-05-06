137fd8f2f72e3fcff85928754702fe078ac5f93f
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-06 09:07:35 -0400

    Update and rename countVowels.exs to count_vowels.exs

---
+++ count_vowels.exs
@@ -0,0 +1,12 @@
+defmodule MyModule do
+  @vowels ["a", "e", "i", "o", "u"]
+  
+  def count_vowels(word) do
+    word
+    |> String.graphemes()
+    |> Enum.filter(fn char -> char in @vowels end)
+    |> Enum.count()
+  end
+end
+
+MyModule.countVowels("abbacus")

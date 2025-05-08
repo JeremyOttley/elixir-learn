7fc57c0b08355074ef8b8e452f16fe0f845e0423
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-08 11:25:11 -0400

    Update count_vowels.exs

--- count_vowels.exs
+++ count_vowels.exs
@@ -1,6 +1,6 @@
 defmodule MyModule do
   defp is_vowel(c) when c in ~w(a e i o u A E I O U), do: true # `in` is an erlang convention
-  defp is_vowel(_), do: false
+  defp is_vowel(_), do: false # `do:` pattern matching format needs all cases met
 
   def count_vowels(word) do
     word

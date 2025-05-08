4e169c961ac5ba06c8ec0fe5328a8c0e4e2bcde2
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-08 08:38:21 -0400

    Update count_vowels.exs

--- count_vowels.exs
+++ count_vowels.exs
@@ -1,10 +1,11 @@
 defmodule MyModule do
   @vowels ["a", "e", "i", "o", "u"]
-  
+  #defp is_vowel?(c) when c in ["a", "A", "E", "e", "i", "I", "O", "o", "U", "u"], do: true
   def count_vowels(word) do
     word
     |> String.graphemes()
     |> Enum.filter(fn char -> char in @vowels end)
+    #|> Enum.filter(fn char -> is_vowel?(char) end)
     |> Enum.count()
   end
 end

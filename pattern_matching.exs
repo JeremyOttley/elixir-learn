e743add868bb1ead234cfb299f6bd1926900a4e2
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-08 11:37:02 -0400

    Update pattern_matching.exs

--- pattern_matching.exs
+++ pattern_matching.exs
@@ -1,8 +1,9 @@
-def divide(a, b) when b == 0, do: {:error, "division by zero"}
-def divide(a, b), do: {:ok, a / b}
-
+defmodule MyModule do
+  def divide(_, b) when b == 0, do: {:error, "division by zero"}
+  def divide(a, b), do: {:ok, a / b}
+end
 # Usage
-case divide(10, 2) do
+case MyModule.divide(10, 2) do
   {:ok, result} -> IO.puts("Success: #{result}")
   {:error, reason} -> IO.puts("Error: #{reason}")
 end

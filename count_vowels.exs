eabd4100819a3fb343555f87e0952f883122d053
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-08 11:24:46 -0400

    Update count_vowels.exs

--- count_vowels.exs
+++ count_vowels.exs
@@ -1,13 +1,14 @@
 defmodule MyModule do
-  @vowels ["a", "e", "i", "o", "u"]
-  #defp is_vowel?(c) when c in ["a", "A", "E", "e", "i", "I", "O", "o", "U", "u"], do: true
+  defp is_vowel(c) when c in ~w(a e i o u A E I O U), do: true # `in` is an erlang convention
+  defp is_vowel(_), do: false
+
   def count_vowels(word) do
     word
     |> String.graphemes()
-    |> Enum.filter(fn char -> char in @vowels end)
-    #|> Enum.filter(fn char -> is_vowel?(char) end)
+    |> Enum.filter(&is_vowel/1)
     |> Enum.count()
   end
 end
 
-MyModule.count_vowels("abbacus")
+x = MyModule.count_vowels("Jeremy")
+IO.puts("Your String has #{x} vowels")

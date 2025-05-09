1356ef9a95b735ab7c4bab9a4981a0e5d364525f
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-09 11:16:59 -0400

    Update bat_avg_rating.exs

--- bat_avg_rating.exs
+++ bat_avg_rating.exs
@@ -1,12 +1,15 @@
 defmodule Baseball do
   def batAvgRating(hits, atBats) do
     case hits / atBats do
-      avg when avg <= 0.200 -> "Turrible"
-      avg when avg >= 0.250 -> "Aiight"
-      avg when avg <= 0.280 -> "Niiiiiiice"
-      _ -> "JFC!"
+      avg when avg <= 0.200 -> {:ok, "Turrible"}
+      avg when avg <= 0.250 -> {:ok, "Aiight"}
+      avg when avg <= 0.280 -> {:ok, "Niiiiiiice"}
+      _ -> {:error, "JFC!"}
     end
   end
 end
 
-Baseball.batAvgRating(20, 100)
+case Baseball.batAvgRating(20, 100) do
+  {:ok, result} -> result
+  {:error, reason} -> reason
+end

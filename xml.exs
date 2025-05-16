c5a2c85925e2406ea1af38aeff324faf54233c5e
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-16 12:43:18 -0400

    Update xml.exs

--- xml.exs
+++ xml.exs
@@ -1,3 +1,9 @@
+# defp deps() do
+#   [
+#     {:exml, "~> 0.1.3"}
+#   ]
+# end
+
 Mix.install([
 {:exml, "~> 0.1.3"}
 ])
@@ -7,21 +13,25 @@ Mix.install([
 # ])
 
 xml = """
-<?xml version="1.0" encoding="UTF-8"?>
-<bookstore>
-    <book>
-        <title lang="en">Harry Potter</title>
-        <price>29.99</price>
-    </book>
-    <book>
-        <title lang="en">Learning XML</title>
-        <price>39.95</price>
-    </book>
-</bookstore> 
+<XML>
+<xml>
+<publisher>
+<publisher_name>University of Illinois Press</publisher_name>
+<prefix_name>University of Illinois Press</prefix_name>
+<publisher_location>Champaign, IL, United States</publisher_location>
+<member_id>3673</member_id>
+<member_type>Member Publisher</member_type>
+<annual_fee>550</annual_fee>
+<date_joined>2011-04-07</date_joined>
+<other_prefixes>
+<prefix>10.5622</prefix>
+<prefix>10.5406</prefix>
+</other_prefixes>
+</publisher>
+</xml>
+</XML>
 """
 
 doc = Exml.parse xml
 
-Exml.get doc, "//book[1]/title/@lang"
-
-IO.puts Exml.get doc, "//title"
+IO.puts Exml.get doc, "//publisher_name"

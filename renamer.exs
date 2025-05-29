1ce23fd75df7b3bb40964b58e8bd828321873f46
Jeremy Ottley <Gazaunga@gmail.com>
2025-05-29 09:00:05 -0400

    Update renamer.exs

--- renamer.exs
+++ renamer.exs
@@ -1,3 +1,9 @@
+# Get current directory
+current_dir = File.cwd!()
+
+# Function to recursively find all files
+my_files = File.ls!(current_dir)
+
 defmodule Renamer do
   @extensions ["jpg", "bmp", "webp", "jxl", "jpeg", "png"]
   @files ["file.jpg", "file.bmp", "file.webp", "file.jxl", "file.jpeg", "file.png"]
